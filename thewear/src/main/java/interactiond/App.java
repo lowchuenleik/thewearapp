c317a2ece8e5f6063111d0c0fd135e55498416f6
Chuen Leik <chuenleik_3837@hotmail.com>
2019-05-09 15:42:54 +0100

    Initial maven commit, plain app

---
+++ thewear/src/main/java/interactiond/App.java
@@ -0,0 +1,16 @@
+package interactiond;
+
+/**
+ * Hello world!
+ *
+ */
+
+ //java -classpath "target/thewear1.0.jar;./target/classes" interactiond.App
+ 
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
