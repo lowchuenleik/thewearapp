fa0e921b2402f8923c6b2dbb4e65f6d3e883e642
isylynch <isylynch@hotmail.com>
2019-05-14 14:35:46 +0100

    added placeholder getter and setter methods for all attributes so that can work easily with Today

--- ID_Code/src/Summary.java
+++ ID_Code/src/Summary.java
@@ -1,15 +1,77 @@
 public class Summary {
-<<<<<<< Updated upstream
-  private int rainProbability;
-  private int weatherCode;
-  private int highTemp;
-  private int averageTemp;
-  private int lowTemp;
-  private int apparentTemp;
-  private String partOfDay;
-  private int cloudCoverage;
+    private int rainProbability;
+    private int weatherCode;
+    private int highTemp;
+    private int averageTemp;
+    private int lowTemp;
+    private int apparentTemp;
+    private String partOfDay;
+    private int cloudCoverage;
 
-  //TODO: make rest of set & get statements
-  public int getRain() { return rainProbability };
+    //TODO: make rest of set & get statements
+    public int getRain() { return rainProbability };
 
+    public int getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(int rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+
+    public int getWeatherCode() {
+        return weatherCode;
+    }
+
+    public void setWeatherCode(int weatherCode) {
+        this.weatherCode = weatherCode;
+    }
+
+    public int getHighTemp() {
+        return highTemp;
+    }
+
+    public void setHighTemp(int highTemp) {
+        this.highTemp = highTemp;
+    }
+
+    public int getAverageTemp() {
+        return averageTemp;
+    }
+
+    public void setAverageTemp(int averageTemp) {
+        this.averageTemp = averageTemp;
+    }
+
+    public int getLowTemp() {
+        return lowTemp;
+    }
+
+    public void setLowTemp(int lowTemp) {
+        this.lowTemp = lowTemp;
+    }
+
+    public int getApparentTemp() {
+        return apparentTemp;
+    }
+
+    public void setApparentTemp(int apparentTemp) {
+        this.apparentTemp = apparentTemp;
+    }
+
+    public String getPartOfDay() {
+        return partOfDay;
+    }
+
+    public void setPartOfDay(String partOfDay) {
+        this.partOfDay = partOfDay;
+    }
+
+    public int getCloudCoverage() {
+        return cloudCoverage;
+    }
+
+    public void setCloudCoverage(int cloudCoverage) {
+        this.cloudCoverage = cloudCoverage;
+    }
 }
