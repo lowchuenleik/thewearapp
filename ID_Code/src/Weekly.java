9829450846de7540b7aae4f37659fab85eb76ab3
peractio <30549145+peractio@users.noreply.github.com>
2019-05-16 21:59:06 +0100

    included instructions on preferred methods to include

--- ID_Code/src/Weekly.java
+++ ID_Code/src/Weekly.java
@@ -1,9 +1,26 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class Weekly {
     private ArrayList<Summary> days;
+
+    // TODO: Yulong here. Can you try including three getter methods, as described below?
+    // Preferably String so I can just show them on the GUI
+    // Otherwise, List<Integer> works, like you said, integer 0 means Sunday, integer 6 means Saturday
+    public List<String> getDayNames(){
+        return null;
+    }
+    // in Celsius
+    public List<Integer> getTemperatures(){
+        return null;
+    }
+    // in our mockup, we showed clothes icons I think, but I don't think it's too important to follow that if weather is much easier or something
+    public List<File> getWeatherIcons(){
+        return null;
+    }
 
     public Weekly() {
         days = new ArrayList<>();
