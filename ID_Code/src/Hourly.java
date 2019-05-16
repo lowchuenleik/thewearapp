c10426fa99f82954bf813ad843c23931b2c9dabb
peractio <30549145+peractio@users.noreply.github.com>
2019-05-16 21:59:14 +0100

    included instructions on preferred methods to include

--- ID_Code/src/Hourly.java
+++ ID_Code/src/Hourly.java
@@ -1,10 +1,25 @@
 import java.io.File;
+import java.util.List;
 
 public class Hourly {
     private int hour;
     private int temp;
     private File weathericon;
     private int weathercode;
+
+    // TODO: Yulong here. Can you try including three getter methods, as described below? (@Chuen: you should be able to easily get the hours and temp from the hourly forecast)
+    // preferably String so I can just show them e.g. "01:00" or "13:00"
+    // Otherwise, List<Integer> also works, e.g. integer 1 means 1am, integer 23 means 11pm
+    public List<String> getHours(){
+        return null;
+    }
+    // in Celsius
+    public List<Integer> getTemperatures(){
+        return null;
+    }
+    public List<File> getWeatherIcons(){
+        return null;
+    }
 
     public Hourly(int hour) {
         this.hour = hour;
