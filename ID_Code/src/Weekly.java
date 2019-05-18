b48d816fb43e46c7fe6208cb245128d2a51f416e
isylynch <isylynch@hotmail.com>
2019-05-19 00:10:15 +0100

    completed all requested functions in the weekly class and commented

--- ID_Code/src/Weekly.java
+++ ID_Code/src/Weekly.java
@@ -1,51 +1,85 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class Weekly {
     private ArrayList<Summary> days;
 
-    // TODO: Yulong here. Can you try including three getter methods, as described below?
-    // Preferably String so I can just show them on the GUI
-    // Otherwise, List<Integer> works, like you said, integer 0 means Sunday, integer 6 means Saturday
-    public List<String> getDayNames(){
-        return null;
-    }
-    // in Celsius
-    public List<Integer> getTemperatures(){
-        return null;
-    }
-    // in our mockup, we showed clothes icons I think, but I don't think it's too important to follow that if weather is much easier or something
-    public List<File> getWeatherIcons(){
-        return null;
-    }
-
+    //CONSTRUCTOR: Weekly stores all of the days of the week as Summary objects
     public Weekly() {
         days = new ArrayList<>();
 
         for (int i=0; i<7; i++) {
             Summary dayi = new Summary(i);
-
             days.add(i, dayi);
         }
         this.days = days;
     }
 
-    public ArrayList<Summary> getDays() {
+    //ARGUMENTS: none
+    //RETURNS: a list of all the days as Summary objects so that the main
+    // screen can easily be rendered for any of the days
+    public List<Summary> getDaySummaries() {
         return days;
     }
 
-    public void updateDays(ArrayList<Summary> days) {
+    //ARGUMENTS: none
+    //RETURNS: none
+    //This refreshes all the data held in the week object so that it is up-to-date
+    public void updateWeek() {
         for (int i=0; i<7; i++) {
             Summary dayi = new Summary(i);
-
             days.set(i, dayi);
         }
     }
 
-    public static void main(String[] args) {
+    //ARGUMENTS: none
+    //RETURNS: a list of the days of the week as strings
+    public List<String> getDayNames(){
+        List<String> daynames = new LinkedList<>();
+        for (Summary d : days) {
+            daynames.add(0, d.getDay()); //prepends the list
+        }
 
+        return daynames;
+    }
+
+    //ARGUMENTS: none
+    //RETURNS: a list of all the temperatures as integers in degrees celsius for
+    // each of the days of the week (corresponding to the getDayNames() list)
+    public List<Integer> getTemperatures(){
+        List<Integer> daytemps = new LinkedList<>();
+        for (Summary d : days) {
+            daytemps.add(0, d.getAverageTemp());
+        }
+
+        return daytemps;
+    }
+
+    //ARGUMENTS: none
+    //RETURNS: a list of all the files of the icons that represent the weather
+    // for each of the days of the week (corresponding to the getDayNames() list)
+    public List<File> getWeatherIcons(){
+        List<File> dayicons = new LinkedList<>();
+        for (Summary d : days) {
+            int wc = d.getWeatherCode();
+
+            File icon = null;
+            switch (wc) {
+                case 0: icon = new File("src/data/hourlyicons/sunny.png");
+                    break;
+                case 1: icon = new File("src/data/hourlyicons/windy.png");
+                    break;
+                case 2: icon = new File("src/data/hourlyicons/cloudy.png");
+                    break;
+                case 3: icon = new File("src/data/hourlyicons/rainy.png");
+                    break;
+                case 4: icon = new File("src/data/hourlyicons/snowy.png");
+                    break;
+            }
+
+            dayicons.add(0, icon);
+        }
+
+        return dayicons;
     }
 }
