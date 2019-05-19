1e8c4f10afa096a49e7be3d61b27533a7a95fe2e
Chuen Leik <chuenleik_3837@hotmail.com>
2019-05-19 23:52:24 +0100

    Fixed bugs

--- ID_Code/src/Hourly.java
+++ ID_Code/src/Hourly.java
@@ -1,9 +1,15 @@
 import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.List;
 import java.time.LocalDate;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.LinkedList;
 
 public class Hourly {
     private int hour;
@@ -17,9 +23,11 @@ public class Hourly {
     // Otherwise, List<Integer> also works, e.g. integer 1 means 1am, integer 23 means 11pm
     public List<String> getHours(){
         List<Map<String,String>> data = hourlydata_augmented();//24 hour time
-        List<Integer> output = new LinkedList<>();
+        List<String> output = new LinkedList<>();
         for (Map<String,String> entry: data){
-            output.add(0,(int)Double.parseDouble(selected_hour.get("hour")));//preprends it!
+            temp = (int)Double.parseDouble(entry.get("hour"));
+            Integer twentyfourhtime = Integer.parseInt(new SimpleDateFormat("HH").format(temp));
+            output.add(0,Integer.toString(twentyfourhtime));//preprends it!
         }
         return output;
     }
@@ -28,7 +36,7 @@ public class Hourly {
         List<Map<String,String>> data = hourlydata_augmented();
         List<Integer> output = new LinkedList<>();
         for (Map<String,String> entry: data){
-            output.add(0,(int)Double.parseDouble(selected_hour.get("feelsLikeTemp")));//preprends it!
+            output.add(0,(int)Double.parseDouble(entry.get("feelsLikeTemp")));//preprends it!
         }
         return output;
     }
@@ -43,7 +51,7 @@ public class Hourly {
         List<Map<String,String>> daily_data = null;
         String intime = "";
         Date temp = null;
-        Map<String,String> selected_hour = new Map<String,String>();
+        Map<String,String> selected_hour = new HashMap<String,String>();
         try{
             daily_data = api_instance.getDaily();//hourly, 24 elements
         } catch (IOException e){
@@ -51,9 +59,13 @@ public class Hourly {
         }
         for (Map<String,String> entry: daily_data){
             intime = entry.get("localTime");
-            temp = new SimpleDateFormat("YYYY-MM-DD'T'HH:mm:ss").parse(intime);
+            try{
+                temp = new SimpleDateFormat("YYYY-MM-DD'T'HH:mm:ss").parse(intime);
+            } catch (ParseException e){
+                System.out.println("ERROR, PLEASE DEBUG");
+            }
             Integer twentyfourhtime = Integer.parseInt(new SimpleDateFormat("HH").format(temp));
-            entry.put("hour",twentyfourhtime);
+            entry.put("hour",twentyfourhtime.toString());
             if (twentyfourhtime==hour){
                 selected_hour = entry;
             }
@@ -79,7 +91,7 @@ public class Hourly {
 
     public void updateTemp() {
         Map<String,String> freshdata = refreshdata();
-        this.temp = freshdata.get("feelsLikeTemp"); 
+        this.temp = (int)Double.parseDouble(freshdata.get("feelsLikeTemp")); 
     }
 
     public File getWeathericon() {
@@ -94,13 +106,13 @@ public class Hourly {
     }
     public void setWeathercode() {
         Map<String,String> freshdata = refreshdata();
-        this.weathercode = freshdata.get("weather_code");
+        this.weathercode = (int)Double.parseDouble(freshdata.get("weather_code"));
     }
 
     public Map<String,String> refreshdata(){
         API api_instance = new API();
         List<Map<String,String>> daily_data = new LinkedList<>();
-        Map<String,String> selected_hour = new Map<String,String>();
+        Map<String,String> selected_hour = new HashMap<String,String>();
         String intime = "";
         Date temp = null;
         try{
@@ -110,9 +122,13 @@ public class Hourly {
         }
         for (Map<String,String> entry: daily_data){
             intime = entry.get("localTime");
-            temp = new SimpleDateFormat("YYYY-MM-DD'T'HH:mm:ss").parse(intime);
+            try{
+                temp = new SimpleDateFormat("YYYY-MM-DD'T'HH:mm:ss").parse(intime);
+            } catch (ParseException e){
+                System.out.println("ERROR, PLEASE DEBUG");
+            }
             Integer twentyfourhtime = Integer.parseInt(new SimpleDateFormat("HH").format(temp));
-            entry.put("hour",twentyfourhtime);
+            entry.put("hour",twentyfourhtime.toString());
             if (twentyfourhtime==hour){
                 selected_hour = entry;
             }
@@ -132,9 +148,13 @@ public class Hourly {
         }
         for (Map<String,String> entry: daily_data){
             intime = entry.get("localTime");
-            temp = new SimpleDateFormat("YYYY-MM-DD'T'HH:mm:ss").parse(intime);
+            try{
+                temp = new SimpleDateFormat("YYYY-MM-DD'T'HH:mm:ss").parse(intime);
+            } catch (ParseException e){
+                System.out.println("ERROR, PLEASE DEBUG");
+            }
             Integer twentyfourhtime = Integer.parseInt(new SimpleDateFormat("HH:mm").format(temp));
-            entry.put("hour",twentyfourhtime);
+            entry.put("hour",twentyfourhtime.toString());
         }
         return daily_data;
     }
